6d9a585ac4d37e02505d3b4339b3fcee3ee50509
3djc <lesitewebdejc@hotmail.com>
2021-04-23 10:01:27 +0200

    Update build-companion-nightly.sh

--- tools/build-companion-nightly.sh
+++ tools/build-companion-nightly.sh
@@ -36,7 +36,8 @@ if [ "$3" != "" ]; then
   COMMON_OPTIONS="${COMMON_OPTIONS} -DVERSION_SUFFIX=$3"
 else
   wget https://downloads.open-tx.org/2.3/nightlies/companion/companion-linux.stamp
-  version=$(grep -oP '(?<=\wN)\w+' companion-linux.stamp)
+  version=$(cut -d'N' -f 3 < companion-linux.stam)
+  version=$(echo $version | tr -cd '[[:digit:]]')
   version=$((version+1))
   COMMON_OPTIONS="${COMMON_OPTIONS} -DVERSION_SUFFIX=$version"
 fi
